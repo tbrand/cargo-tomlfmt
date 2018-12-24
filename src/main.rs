1d4b459a9d2e48fefe63e0d145e3e798a442b137
tbrand <taichiro0709@gmail.com>
2018-12-24 23:24:28 +0900

    add tests

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,33 @@ type Result<T> = std::result::Result<T, failure::Error>;
 mod cli;
 mod fmt;
 
+fn fmt_toml(orig: &str) -> Result<String> {
+    let mut doc = orig.parse::<toml_edit::Document>()?;
+    let doc_keys = doc.clone();
+    let keys = doc_keys
+        .as_table()
+        .iter()
+        .map(|(key, _)| key.to_owned())
+        .collect::<Vec<String>>();
+
+    for key in keys.iter() {
+        if key == "package" {
+            // we don't format the 'package' table.
+            continue;
+        }
+
+        if doc[key].is_table() {
+            fmt::fmt_table(&mut doc[key.as_str()].as_table_mut().unwrap())?;
+        } else if doc[key].is_array_of_tables() {
+            fmt::fmt_array_of_tables(&mut doc[key.as_str()].as_array_of_tables_mut().unwrap())?;
+        } else if doc[key].is_value() {
+            fmt::fmt_value(&mut doc[key.as_str()].as_value_mut().unwrap())?;
+        }
+    }
+
+    Ok(doc.to_string())
+}
+
 fn main() -> Result<()> {
     if std::env::var("RUST_LOG").is_err() {
         std::env::set_var("RUST_LOG", "info");
@@ -74,31 +101,7 @@ fn main() -> Result<()> {
 
     let file = std::fs::read(path)?;
     let orig = std::str::from_utf8(file.as_slice())?;
-
-    let mut doc = orig.parse::<toml_edit::Document>()?;
-    let doc_keys = doc.clone();
-    let keys = doc_keys
-        .as_table()
-        .iter()
-        .map(|(key, _)| key.to_owned())
-        .collect::<Vec<String>>();
-
-    for key in keys.iter() {
-        if key == "package" {
-            // we don't format the 'package' table.
-            continue;
-        }
-
-        if doc[key].is_table() {
-            fmt::fmt_table(&mut doc[key.as_str()].as_table_mut().unwrap())?;
-        } else if doc[key].is_array_of_tables() {
-            fmt::fmt_array_of_tables(&mut doc[key.as_str()].as_array_of_tables_mut().unwrap())?;
-        } else if doc[key].is_value() {
-            fmt::fmt_value(&mut doc[key.as_str()].as_value_mut().unwrap())?;
-        }
-    }
-
-    let formatted = doc.to_string();
+    let formatted = fmt_toml(&orig)?;
 
     if orig != formatted {
         if flag_dryrun {
@@ -126,4 +129,31 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn parse_inline_table() {
+        let file = std::fs::read("test/fixtures/value_after_table.toml").unwrap();
+        let file_str = std::str::from_utf8(file.as_slice()).unwrap();
+
+        assert!(fmt_toml(&file_str).is_ok());
+    }
+
+    #[test]
+    fn preserve_comments() {
+        let file = std::fs::read("test/fixtures/keep_comment.toml").unwrap();
+        let file_str = std::str::from_utf8(file.as_slice()).unwrap();
+        let formatted = fmt_toml(&file_str);
+
+        assert!(formatted.is_ok());
+
+        let formatted = formatted.unwrap();
+
+        assert!(formatted.contains("# this is an inline comment"));
+        assert!(formatted.contains("# this is a suffix comment"));
+    }
 }
