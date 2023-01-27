8815b7f21f03d3725d92be9a743cefc4150a26f7
Rafael Bachmann <rafael.bachmann@esrlabs.com>
2023-01-27 18:00:02 +0100

    Use derive style in clap4

--- src/cli.rs
+++ src/cli.rs
@@ -1,60 +1,32 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, Arg, Command};
+use std::path::PathBuf;
 
-pub fn app() -> Command {
-    clap::Command::new(crate_name!())
-        .version(crate_version!())
-        .about(crate_description!())
-        .author(crate_authors!())
-        .subcommand(
-            clap::Command::new("tomlfmt")
-                .about("It's just an alias as a subcommand of cargo.")
-                .arg(arg_path())
-                .arg(arg_dry_run())
-                .arg(arg_keep())
-                .arg(arg_create()),
-        )
-        .arg(arg_path())
-        .arg(arg_dry_run())
-        .arg(arg_keep())
-        .arg(arg_create())
-}
+use clap::{command, Parser};
 
-pub fn arg_path() -> Arg {
-    Arg::new("path")
-        .long("path")
-        .short('p')
-        .help("Path to the manifest. (default is Cargo.toml)")
-        .action(clap::ArgAction::Set)
-}
+#[derive(Debug, Parser)]
+#[command(version, author)]
+pub struct Arguments {
+    /// Path to the manifest.
+    #[arg(long, short = 'p', default_value = "Cargo.toml")]
+    pub path: PathBuf,
 
-pub fn arg_dry_run() -> Arg {
-    Arg::new("dryrun")
-        .long("dryrun")
-        .short('d')
-        .help("Do NOT overwrite the file.")
-        .action(clap::ArgAction::SetTrue)
-}
+    /// Do NOT overwrite the file.
+    #[arg(long, short = 'd', default_value_t = false)]
+    pub dryrun: bool,
 
-pub fn arg_keep() -> Arg {
-    Arg::new("keep")
-        .long("keep")
-        .short('k')
-        .help("Keep the original manifest as Cargo.toml.bak.")
-        .action(clap::ArgAction::SetTrue)
-}
+    /// Keep the original manifest as Cargo.toml.bak.
+    #[arg(long, short = 'k', default_value_t = false)]
+    pub keep: bool,
 
-pub fn arg_create() -> Arg {
-    Arg::new("create")
-        .long("create")
-        .short('c')
-        .help("Create a formatted manifest as Cargo.toml.new when dryrun.")
-        .action(clap::ArgAction::SetTrue)
+    /// Create a formatted manifest as Cargo.toml.new when dryrun.
+    #[arg(long, short = 'c', default_value_t = false)]
+    pub create: bool,
 }
 
 #[cfg(test)]
 mod test {
     #[test]
-    fn verify_app() {
-        super::app().debug_assert();
+    fn verify_cli() {
+        use clap::CommandFactory;
+        super::Arguments::command().debug_assert()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use clap::Parser;
+use cli::Arguments;
+
 mod cli;
 mod fmt;
 
@@ -37,60 +40,18 @@ fn main() -> Result<()> {
 
     env_logger::init();
 
-    let app = cli::app();
-    let matches = app.get_matches();
+    let args = Arguments::parse();
+    dbg!(&args);
 
-    let mut path = matches.get_one::<String>("path").map(|s| s.as_str());
-
-    if path.is_none() {
-        path = matches
-            .subcommand_matches("tomlfmt")
-            .and_then(|m| m.get_one::<String>("path").map(|s| s.as_str()));
-    }
-
-    let path = path.unwrap_or("Cargo.toml");
-
-    let flag_dryrun = matches.contains_id("dryrun")
-        || matches
-            .subcommand_matches("tomlfmt")
-            .map(|m| m.contains_id("dryrun"))
-            .unwrap_or(false);
-    let flag_keep = matches.contains_id("keep")
-        || matches
-            .subcommand_matches("tomlfmt")
-            .map(|m| m.contains_id("keep"))
-            .unwrap_or(false);
-    let flag_create = matches.contains_id("create")
-        || matches
-            .subcommand_matches("tomlfmt")
-            .map(|m| m.contains_id("create"))
-            .unwrap_or(false);
-
-    if flag_dryrun {
-        log::debug!("flag: dryrun");
-    }
-
-    if flag_keep {
-        log::debug!("flag: keep");
-    }
-
-    if flag_create {
-        log::debug!("flag: create");
-
-        if !flag_dryrun {
-            log::warn!("flag: create can be used with dryrun");
-        }
-    }
-
-    let file = std::fs::read(path)?;
+    let file = std::fs::read(&args.path)?;
     let orig = std::str::from_utf8(file.as_slice())?;
     let formatted = fmt_toml(orig)?;
 
     if orig != formatted {
-        if flag_dryrun {
+        if args.dryrun {
             log::warn!("dryrun founds problems in Cargo.toml");
 
-            if flag_create {
+            if args.create {
                 log::info!("create Cargo.toml.new");
                 std::fs::write("Cargo.toml.new", formatted)?;
             }
@@ -99,10 +60,10 @@ fn main() -> Result<()> {
             std::process::exit(-1);
         } else {
             log::info!("overwrite the manifest");
-            std::fs::rename(path, "Cargo.toml.bak")?;
-            std::fs::write(path, formatted)?;
+            std::fs::rename(&args.path, "Cargo.toml.bak")?;
+            std::fs::write(args.path, formatted)?;
 
-            if !flag_keep {
+            if !args.keep {
                 std::fs::remove_file("Cargo.toml.bak")?;
             }
         }
